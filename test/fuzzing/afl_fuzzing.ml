987b50d81686ca8caf65968a7587ea7fdc9df4e9
hichaeh <hichem.rami.aitelhara@gmail.com>
2021-03-29 09:03:30 +0200

    Added uninterpreted symbols (variables, functions, predicates)

--- test/fuzzing/afl_fuzzing.ml
+++ test/fuzzing/afl_fuzzing.ml
@@ -20,10 +20,10 @@ module PA = Parsed_interface
 
 let thmid = ref 0
 
-let max_fuel = 5
+let max_fuel = 2
 
 (** Generator of a constant boolean Expr.t *)
-let gen_cst_bexpr = 
+let gen_cst_bool_expr () = 
   Crowbar.(
     with_printer Expr.print @@ 
       map [bool] 
@@ -38,16 +38,39 @@ let gen_cst_int_expr () =
       map [int] 
       (fun i -> Expr.int (Int.to_string i)))
 
-
 (** Generator of a floating point number Expr.t *)
 let gen_cst_real_expr () =
   Crowbar.(
     with_printer Expr.print @@ 
       map [float] (fun i -> 
             if Float.is_nan i
-            then  Expr.real "0." 
+            then Expr.real "0." 
             else Expr.real (Float.to_string i)))
 
+let nb_vars = 3
+
+let get_var_names (ty : Ty.t) = 
+  match ty with 
+  | Ty.Tint -> ["iv1"; "iv2"; "iv3"]
+  | Ty.Treal -> ["rv1"; "rv2"; "rv3"]
+  | Ty.Tbool -> ["bv1"; "bv2"; "bv3"]
+  | _ -> assert false 
+
+let get_func_names (ty : Ty.t) = 
+  match ty with 
+  | Ty.Tint ->
+    [ [Ty.Tint], "if1";
+      [Ty.Tint; Ty.Treal], "if2";
+      [Ty.Tint; Ty.Treal; Ty.Tbool], "if3"]
+  | Ty.Treal ->
+    [ [Ty.Tint], "rf1";
+      [Ty.Tint; Ty.Treal], "rf2";
+      [Ty.Tint; Ty.Treal; Ty.Tbool], "rf3"]
+  | Ty.Tbool ->
+    [ [Ty.Tint], "bf1";
+      [Ty.Tint; Ty.Treal], "bf2";
+      [Ty.Tint; Ty.Treal; Ty.Tbool], "bf3"]
+  | _ -> assert false
 
 let arithops (ty : Ty.t) =
   List.map (fun x -> Symbols.Op x) @@
@@ -56,8 +79,7 @@ let arithops (ty : Ty.t) =
     | Ty.Tint -> Symbols.Modulo::l
     | Ty.Treal -> l
     | _ -> assert false
-   
-  
+
 (*** Generates an expr of type ty *)
 let rec gen_iexpr ?(fuel = max_fuel) ?(ty = Ty.Tint) () =
   Crowbar.with_printer Expr.print @@
@@ -65,8 +87,12 @@ let rec gen_iexpr ?(fuel = max_fuel) ?(ty = Ty.Tint) () =
     | true -> 
       begin 
         match ty with 
-        | Ty.Tint -> gen_cst_int_expr ()
-        | Ty.Treal -> gen_cst_real_expr ()
+        | Ty.Tint -> 
+          Crowbar.choose
+            [ gen_cst_int_expr (); gen_var ty] 
+        | Ty.Treal ->
+          Crowbar.choose 
+            [ gen_cst_real_expr (); gen_var ty]
         | _ -> assert false
       end  
     | false ->
@@ -78,20 +104,22 @@ let rec gen_iexpr ?(fuel = max_fuel) ?(ty = Ty.Tint) () =
             (fun a b -> Expr.mk_term op [a;b] ty))::acc)
       in 
       Crowbar.choose @@
+        gen_fun fuel ty @
         List.fold_left fn [gen_iexpr ~fuel:(fuel-1) ~ty ()] (arithops ty)
 
-      
 (** Generator of a Expr.t of type bool/prop and of max depth max_fuel *)
-let rec gen_expr_prop ?(fuel = max_fuel) () = 
+and gen_expr_prop ?(fuel = max_fuel) () = 
   Crowbar.with_printer Expr.print @@
     match fuel = 0 with
-    | true -> gen_cst_bexpr
+    | true -> gen_cst_bool_expr ()
     | false ->
       Crowbar.choose @@
       
-      gen_expr_prop ~fuel:(fuel-1) () :: 
+      gen_expr_prop ~fuel:(fuel-1) ()
+      
+      :: gen_fun fuel Ty.Tbool
 
-      List.map 
+      @ List.map 
       ( Crowbar.map [ gen_expr_prop ~fuel:(fuel-1) (); 
                       gen_expr_prop ~fuel:(fuel-1) ()])
       [ (fun a b -> Expr.mk_and a b true 0);
@@ -100,14 +128,10 @@ let rec gen_expr_prop ?(fuel = max_fuel) () =
         (fun a b -> Expr.mk_iff a b 0);
         (fun a b -> Expr.mk_xor a b 0);]
 
-
       @ List.map 
-      ( (* let t = Array.get [|Ty.Tint; Ty.Treal|] (Random.int 2) in *)
-        (* let ty = if Random.bool () then Ty.Tint else Ty.Treal in *) 
-        Crowbar.map [ gen_iexpr ~fuel:(fuel-1) ~ty:Ty.Tint (); 
+      ( Crowbar.map [ gen_iexpr ~fuel:(fuel-1) ~ty:Ty.Tint (); 
                       gen_iexpr ~fuel:(fuel-1) ~ty:Ty.Tint ()])
-      [ 
-        (fun a b -> Expr.mk_eq ~iff:true a b);
+      [ (fun a b -> Expr.mk_eq ~iff:true a b);
         (fun a b -> Expr.mk_eq ~iff:false a b);
         (fun a b -> Expr.mk_distinct ~iff:true [a;b]);
         (fun a b -> Expr.mk_distinct ~iff:false [a;b]);
@@ -122,11 +146,9 @@ let rec gen_expr_prop ?(fuel = max_fuel) () =
           Expr.mk_builtin ~is_pos:false Symbols.LT [a;b]);]
         
      @ List.map 
-      ( 
-        Crowbar.map [ gen_iexpr ~fuel:(fuel-1) ~ty:Ty.Treal (); 
+      ( Crowbar.map [ gen_iexpr ~fuel:(fuel-1) ~ty:Ty.Treal (); 
                       gen_iexpr ~fuel:(fuel-1) ~ty:Ty.Treal ()])
-      [ 
-        (fun a b -> Expr.mk_eq ~iff:true a b);
+      [ (fun a b -> Expr.mk_eq ~iff:true a b);
         (fun a b -> Expr.mk_eq ~iff:false a b);
         (fun a b -> Expr.mk_distinct ~iff:true [a;b]);
         (fun a b -> Expr.mk_distinct ~iff:false [a;b]);
@@ -140,6 +162,55 @@ let rec gen_expr_prop ?(fuel = max_fuel) () =
         (fun a b ->
           Expr.mk_builtin ~is_pos:false Symbols.LT [a;b]);]
 
+and gen_expr ?(fuel = max_fuel) ~ty = 
+  match ty with 
+  | Ty.Tbool -> gen_expr_prop ~fuel ()
+  | _ -> gen_iexpr ~fuel ~ty ()
+
+and gen_var (ty : Ty.t) =
+  Crowbar.map 
+  [Crowbar.range nb_vars] 
+  ( fun pos -> 
+      let var = List.nth (get_var_names ty) pos 
+      in 
+      Expr.mk_term 
+        (Symbols.Name 
+          (Hstring.make var, Symbols.Other)) 
+        [] ty)
+
+and gen_fun (fuel : int) (ty : Ty.t) =
+  [ Crowbar.map 
+      [ gen_expr ~fuel:(fuel - 1) ~ty:Ty.Tint ]
+      ( fun x ->
+          let f = snd @@ List.nth (get_func_names ty) 0 
+          in 
+          Expr.mk_term 
+            (Symbols.Name 
+              (Hstring.make f, Symbols.Other)) 
+            [x] ty);
+    Crowbar.map 
+      [ gen_expr ~fuel:(fuel - 1) ~ty:Ty.Tint;
+        gen_expr ~fuel:(fuel - 1) ~ty:Ty.Treal]
+      ( fun x y ->
+          let f = snd @@ List.nth (get_func_names ty) 1
+          in 
+          Expr.mk_term 
+            (Symbols.Name 
+              (Hstring.make f, Symbols.Other)) 
+            [x; y] ty);
+    Crowbar.map 
+      [ gen_expr ~fuel:(fuel - 1) ~ty:Ty.Tint;
+        gen_expr ~fuel:(fuel - 1) ~ty:Ty.Treal;
+        (* gen_expr_prop ~fuel:(fuel-1) () *) (*???*)
+        gen_cst_bool_expr ()]
+      ( fun x y z ->
+        let f = snd @@ List.nth (get_func_names ty) 2
+        in 
+        Expr.mk_term 
+          (Symbols.Name 
+            (Hstring.make f, Symbols.Other)) 
+          [x; y; z] ty)
+  ]
 
 (** Makes a Command.Query with goal_sort = theorem from an Expr.t *)
 let mk_cq_thm_from_expr (exp : Expr.t) : Commands.sat_tdecl =
@@ -160,7 +231,8 @@ module FE = Frontend.Make(SAT)
 
 let proc pbs = 
   List.iter
-    (fun (pb, goal_name) -> ignore goal_name;
+    (fun (pb, goal_name) -> 
+      ignore goal_name;
       let _, consistent, ex = 
         List.fold_left
           (fun acc d ->
@@ -173,10 +245,10 @@ let proc pbs =
           pb
       in
       ignore ex; ignore consistent;
-      (*
+      (* 
+      List.iter (Format.printf "\n ###  %s  %a\n" goal_name Commands.print) pb;
       Format.printf "%s@."
-        (if consistent then "consistent(?)\n" else "inconsistent(?)\n")
-      *)
+        (if consistent then "unknown" else "unsat")*)
     ) pbs; 
   true (* for now *)
 
