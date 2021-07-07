bdb94714bf9135f5347581b8b22b372fa2a34121
hichaeh <hichem.rami.aitelhara@gmail.com>
2021-07-07 19:20:16 +0200

    Corrected the afl_fuzzing executable

--- test/fuzzing/afl_fuzzing.ml
+++ test/fuzzing/afl_fuzzing.ml
@@ -8,22 +8,22 @@ let () =
   sh_printf ~firstcall:true ""; 
   Cr.add_test ~name:"ae" 
     [Generator.gen_decls] 
-    ( fun decls -> 
+    ( fun (tydecls, decls) -> 
         Cr.check (
           try
             sh_printf "\n";
             incr cnt;
             let aeres = 
-              AES.process_decls decls
+              AES.process_decls tydecls decls
             in
             let cvc5res = 
-              C5S.process_decls decls
+              C5S.process_decls tydecls decls
             in
             cmp_answers_exn2 aeres cvc5res;
             true
           with
           | exp ->
-            mknmarshall_bi exp decls; 
+            mknmarshall_bi exp tydecls decls; 
             false
         )
     )
