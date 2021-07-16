d233d037af07948efd92ecc2050cb5a474f4c4c6
hichaeh <hichem.rami.aitelhara@gmail.com>
2021-07-16 16:24:43 +0200

    Changed the script that runs afl-fuzz in parallel mode, Now it can do it with a varying number of instances

--- test/fuzzing/afl_pm.sh
+++ test/fuzzing/afl_pm.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 #Runs afl-fuzz in parallel mode
 
-gnome-terminal -- bash -c 'afl-fuzz -t 2000 -m 250 -i ./test/fuzzing/input/ -o ./test/fuzzing/output/sync_dir/ -M fuzzer01  ./_build/default/test/fuzzing/afl_fuzzing.exe  @@; exec bash'
+if [[ $1 =~ ^[0-9]+$ ]] ; then
+  n=$1 
+else 
+  n=0
+fi
 
-gnome-terminal -- bash -c 'afl-fuzz -t 2000 -m 250 -i ./test/fuzzing/input/ -o ./test/fuzzing/output/sync_dir/ -S fuzzer02  ./_build/default/test/fuzzing/afl_fuzzing.exe  @@; exec bash'
+gnome-terminal -- bash -c 'afl-fuzz -t 2000 -m 250 -i ./test/fuzzing/input/ -o ./test/fuzzing/output/sync_dir/ -M fuzzer1  ./_build/default/test/fuzzing/afl_fuzzing.exe  @@; exec bash'
 
-gnome-terminal -- bash -c 'afl-fuzz -t 2000 -m 250 -i ./test/fuzzing/input/ -o ./test/fuzzing/output/sync_dir/ -S fuzzer03  ./_build/default/test/fuzzing/afl_fuzzing.exe  @@; exec bash'
+for ((i = 2; i <= $n; i++ )); do
+  gnome-terminal -- bash -c "afl-fuzz -t 2000 -m 250 -i ./test/fuzzing/input/ -o ./test/fuzzing/output/sync_dir/ -S fuzzer$i ./_build/default/test/fuzzing/afl_fuzzing.exe  @@; exec bash"
+done
 
-gnome-terminal -- bash -c 'afl-fuzz -t 2000 -m 250 -i ./test/fuzzing/input/ -o ./test/fuzzing/output/sync_dir/ -S fuzzer04  ./_build/default/test/fuzzing/afl_fuzzing.exe  @@; exec bash'
