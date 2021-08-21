4511e453c7e5b6dc80dd3f7108c51fb95c86d54d
hichaeh <hichem.rami.aitelhara@gmail.com>
2021-08-21 02:14:28 +0200

    correcte gen settings

--- test/fuzzing/foptions.ml
+++ test/fuzzing/foptions.ml
@@ -1,10 +1,15 @@
 
 
-let u_adts = ref true
+let u_adts = ref false
 
-let u_li = ref true
+let u_li = ref false
 
-let u_ite = ref true 
+let u_ite = ref false 
+
+let u_fa = ref false 
+
+let u_btv = ref false 
+
 
 (* Getters *)
 
@@ -14,10 +19,18 @@ let get_u_li () = !u_li
 
 let get_u_ite () = !u_ite
 
+let get_u_fa () = !u_fa 
+
+let get_u_btv () = !u_btv 
+
 (* Setters *)
 
-let set_u_adts () = !u_adts 
+let set_u_adts = (:=) u_adts 
 
-let set_u_li () = !u_li
+let set_u_li = (:=) u_li
 
-let set_u_ite () = !u_ite
+let set_u_ite = (:=) u_ite
+
+let set_u_fa = (:=) u_fa 
+
+let set_u_btv = (:=) u_btv 
