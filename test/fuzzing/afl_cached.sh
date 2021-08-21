d7f8e10934409df74f295478e1e8b66d48d11c20
hichaeh <hichem.rami.aitelhara@gmail.com>
2021-08-21 12:37:01 +0200

    updated scripts 2

--- test/fuzzing/afl_cached.sh
+++ test/fuzzing/afl_cached.sh
@@ -2,4 +2,7 @@
 
 #Runs afl-fuzz
 
-gnome-terminal -- bash -c 'afl-fuzz -t 5000 -m 300 -i ./test/fuzzing/input/ -o ./test/fuzzing/output/ ./_build/default/test/fuzzing/afl_fuzzing_cached.exe @@; exec bash'
+if [[ $1 =~ ^[0-9]+$ ]]; then t=$1; else t=10000; fi
+if [[ $2 =~ ^[0-9]+$ ]]; then m=$2; else m=500; fi
+
+gnome-terminal -- bash -c "afl-fuzz -t $t -m $m -i ./test/fuzzing/input/ -o ./test/fuzzing/output/ ./_build/default/test/fuzzing/afl_fuzzing_cached.exe @@; exec bash"
