426ab0c66b20fd7f6d550b9851c5a07ea83f4499
hichaeh <hichem.rami.aitelhara@gmail.com>
2021-08-30 03:15:57 +0200

    moved some constants to foptions

--- test/fuzzing/foptions.ml
+++ test/fuzzing/foptions.ml
@@ -1,4 +1,15 @@
 
+let query_max_depth = ref 3
+
+let axiom_max_depth = ref 3
+
+let func_max_depth = ref 3
+
+let nb_us_vars = ref 5
+
+let nb_q_vars = ref 1
+
+(************************************************************)
 
 let u_qvrs = ref true
 
@@ -15,6 +26,18 @@ let u_btv = ref true
 
 (* Getters *)
 
+let get_query_max_depth () = !query_max_depth
+
+let get_axiom_max_depth () = !axiom_max_depth
+
+let get_func_max_depth () = !func_max_depth
+
+let get_nb_us_vars () = !nb_us_vars
+
+let get_nb_q_vars () = !nb_q_vars
+
+(************************************************************)
+
 let get_u_qvrs () = !u_qvrs
 
 let get_u_adts () = !u_adts
@@ -28,6 +51,23 @@ let get_u_fa () = !u_fa
 let get_u_btv () = !u_btv
 
 (* Setters *)
+
+let set_query_max_depth =
+  (:=) query_max_depth
+
+let set_axiom_max_depth =
+  (:=) axiom_max_depth
+
+let set_func_max_depth =
+  (:=) func_max_depth
+
+let set_nb_us_vars =
+  (:=) nb_us_vars
+
+let set_nb_q_vars =
+  (:=) nb_q_vars
+
+(************************************************************)
 
 let set_u_qvrs = (:=) u_qvrs
 
